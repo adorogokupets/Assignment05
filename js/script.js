ca8031802bc3f322074e1c5b1c3f888622bdfee3
Alexey Dorogokupets <adorogokupets@gmail.com>
2024-03-14 16:14:24 -0700

    Employee Management System Completed

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const $ = (id) => document.getElementById(id)
+
+const processForm = () => {
+    const form = $('empForm')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log(`ID: ${$('id').value}`)
+        console.log(`Name: ${$('name').value}`)
+        console.log(`Extension: ${$('ext').value}`)
+        console.log(`Email: ${$('email').value}`)
+        console.log(`Department: ${$('department').value}`)
+    })
+
+}
+processForm()
